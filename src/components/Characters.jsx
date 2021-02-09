c6571fc9d9f3557bd04cd5e7b3a9169ddf7f1046
RoiFuries <roi.furies@gmail.com>
2021-02-08 18:34:43 -0600

    class 8, hook useMemo

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useReducer} from 'react'
+import React, {useState, useEffect, useReducer, useMemo } from 'react'
 
 const initialState = {
     favorites: []
@@ -20,6 +20,8 @@ const Chracters  = () => {
 
     const [characters, setCharacters] = useState([]);
     const [favorites, dispatch] = useReducer(favoriteReducer,initialState)
+    const [search, setSearch] = useState('')
+
 
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character')
@@ -31,6 +33,20 @@ const Chracters  = () => {
         dispatch({type: 'ADD_TO_FAVORITE', payload: favorite})
     }
 
+    const handleSearch = (event) => {
+        setSearch(event.target.value)
+    }
+    // const filteredUsers = characters.filter((user) =>{
+    //     return user.name.toLowerCase().includes(search.toLowerCase());
+    // })
+
+    const filteredUsers = useMemo(() =>
+        characters.filter((user) =>{
+            return user.name.toLowerCase().includes(search.toLowerCase());
+        }),
+        [characters, search]
+    )
+
     return (
         <div className="Chracters">
 
@@ -39,7 +55,12 @@ const Chracters  = () => {
                     {favorite.name}
                 </li>
             ))}
-            {characters.map(character => (
+
+            <div className="Search">
+                <input type="text" value={search} onChange={handleSearch} />
+            </div>
+
+            {filteredUsers.map(character => (
                 <div className="item" key={character.id}>
                     <h2>{character.name}</h2>
                     <button type='button' onClick={() => handleClick(character)}>Agregas a Favs </button>
